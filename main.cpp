dba7dd40544fd6a68fe54183326a23ba474faf6d
svang56 <svang56@mail.csuchico.edu>
2018-02-21 18:59:15 -0800

    Grocery List program

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include<string>
 using namespace std;
 
+const int LISTSIZE = 5; //array of 5 strings
 int main()
 {
-  string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
+  string list[LISTSIZE];
+  int max = LISTSIZE - 1;
+  int total = LISTSIZE;
+
+
   do
   {
  
@@ -16,7 +22,23 @@ int main()
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  
+  if((input == 'a' || input == 'A') && numItems < total)
+  {
+    cout<<"What is the item?"<<endl;
+    cin>>item;
+    list[numItems] = item;
+    numItems++;
+   } 
+  else if( numItems == total && input != 'q' && input != 'Q')
+     {
+     cout<<"You'll need a bigger list!";
+     
+     }
+  
   }while( input != 'Q' && input != 'q');
+
+
 
   return 0;
 }

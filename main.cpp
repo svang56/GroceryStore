a307210306253ca1f4054c1f2926b46f91cfecff
svang56 <svang56@mail.csuchico.edu>
2018-02-21 17:17:31 -0800

    Grocery List

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  do
+  {
+ 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  }while( input != 'Q' && input != 'q');
 
   return 0;
 }

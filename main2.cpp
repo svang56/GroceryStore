79e6ee0c2a38371476ea3341280331a137442cdd
svang56 <svang56@mail.csuchico.edu>
2018-02-21 21:41:01 -0800

    Grocery List

---
+++ main2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+const int LISTSIZE = 5; //array of 5 strings
+int main()
+{
+  int numItems = 0;
+  char input;
+  string item;
+  string list[LISTSIZE];
+  int max = LISTSIZE - 1;
+  int total = LISTSIZE;
+
+
+  do
+  {
+ 
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/Q): ";
+  cin>>input;
+  
+  if((input == 'a' || input == 'A') && numItems <= total)
+  {
+    cout<<"What is the item?"<<endl;
+    cin>>item;
+    if(numItems < total)
+    {
+    list[numItems] = item;
+    }
+
+    if(numItems < total)
+    {
+    numItems++;
+    }
+
+    if( numItems > max)
+    {
+    cout<<"You'll need a bigger list!";
+    }
+  } 
+  
+  }while( input != 'Q' && input != 'q');
+
+
+
+  return 0;
+}
